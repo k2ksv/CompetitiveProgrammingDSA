20031e3a8292928c05b0ae9c8a6d7988b6744ae6
Keshav <keshavkumar1314@gmail.com>
2026-04-21 18:39:46 +0530

    1355A.cpp

---
+++ CodeForces/1355A.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+using namespace std;
+
+
+pair<int, int> getMinMaxDigit(long long number) {
+    int smallestDigit = 9;
+    int largestDigit = 0;
+
+    while (number > 0) {
+        int digit = number % 10;
+
+        if (digit < smallestDigit) {
+            smallestDigit = digit;
+        }
+
+        if (digit > largestDigit) {
+            largestDigit = digit;
+        }
+
+        number = number / 10;
+    }
+
+    return {smallestDigit, largestDigit};
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin >> t;
+
+    while (t--) {
+        long long number, steps;
+        cin >> number >> steps;
+
+        for (int i = 1; i < steps; i++) {
+
+         
+            pair<int, int> result = getMinMaxDigit(number);
+
+            int smallestDigit = result.first;
+            int largestDigit = result.second;
+
+            if (smallestDigit == 0) {
+                break;
+            }
+
+            number = number + (long long)smallestDigit * largestDigit;
+        }
+
+        cout << number << "\n";
+    }
+
+    return 0;
+}
