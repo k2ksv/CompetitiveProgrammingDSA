be728870cb4972b56e9393ba3b1de2f487ab7229
Keshav <keshavkumar1314@gmail.com>
2026-04-22 19:18:42 +0530

    1497B

--- CodeForces/1497B.cpp
+++ CodeForces/1497B.cpp
@@ -12,6 +12,61 @@ int main() {
         int n, m;
         cin >> n >> m;
 
+        vector<int> rm(m, 0);
+       
+        for(int i=0; i<n; i++){
+            int v;
+            cin >> v;
+            int rem = v % m;
+            rm[rem]++;
+        }
+
+        int groups =0;
+        if (rm[0]>0){
+            groups++;
+        }
+
+        for (int r = 1; r <= m/2; r++){
+            if (m-r == r){
+                if (rm[r]>0){
+                    groups++;
+
+                }
+
+            }
+            else {
+                int c1 = rm[r];
+                int c2 = rm[m-r];
+
+                if (c1 == 0 && c2 == 0) continue;
+
+                groups ++;
+                int diff = abs(c1-c2);
+                if (diff > 0){
+                    groups += diff-1;
+
+                }
+
+                
+
+
+
+            }
+
+        }
+
+
+
+        cout << groups <<"\n";
+
+
+
+
+
+
+
+
+
       
        
     }
