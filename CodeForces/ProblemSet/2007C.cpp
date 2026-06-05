b692bdca4047802baed039f95eda6829b1dd1580
k2ksv <keshavkumar1314@gmail.com>
2026-06-05 03:32:09 +0000

    LGM

---
+++ CodeForces/ProblemSet/2007C.cpp
@@ -0,0 +1,74 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll  = long long;
+
+/* debug */
+#ifdef LOCAL
+  #define dbg(x) cerr << #x << " = " << (x) << '\n'
+  #define dbg2(x,y) cerr << #x << "=" << (x) << " " << #y << "=" << (y) << '\n'
+#else
+  #define dbg(x)
+  #define dbg2(x,y)
+#endif
+
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+auto randint(ll l, ll r) { return uniform_int_distribution<ll>(l,r)(rng); }
+
+const ll  INF = 2e18;
+const int MOD = 1e9 + 7;
+
+void Solve() {
+    int n;
+    ll a, b;
+    cin >> n >> a >> b;
+
+    ll g = std::gcd(a, b);
+    vector<ll> c(n);
+    
+    for (int i = 0; i < n; i++) {
+        cin >> c[i];
+        c[i] %= g; 
+    }
+    sort(c.begin(), c.end());
+    ll min_range = c[n - 1] - c[0];
+    for (int i = 0; i < n - 1; i++) {
+        ll current_range = (c[i] + g) - c[i + 1];
+        min_range = min(min_range, current_range);
+    }
+
+    cout << min_range << '\n';
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+#ifndef ONLINE_JUDGE
+    (void)freopen("input.txt",  "r", stdin);
+    (void)freopen("output.txt", "w", stdout);
+#endif
+
+#ifdef LOCAL
+    auto _t0 = chrono::high_resolution_clock::now();
+#endif
+
+    int t = 1;
+    cin >> t;
+    while (t--) { 
+        Solve();
+    }
+
+#ifdef LOCAL
+    auto _t1 = chrono::high_resolution_clock::now();
+    cerr << "\n[time] "
+         << chrono::duration_cast<chrono::milliseconds>(_t1-_t0).count()
+         << " ms\n";
+#endif
+
+    return 0;
+}
+
+/*
+THINK DIFFERENTLY
+*/
